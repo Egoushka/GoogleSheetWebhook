e18758bfce8a9afb6b316a804fc812fd01170496
Egoushka <egorgrabovskij@gmail.com>
2024-11-27 17:50:37 +0200

    Fixed database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,26 @@
-import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Row } from './models/row.model';
+import { CacheModule } from '@nestjs/cache-manager';
 import { Analytics } from './models/analytics.model';
+import { Row } from './models/row.model';
+
+dotenv.config();
 
 @Module({
   imports: [
     CacheModule.register(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '<tembo-host>',
-      port: 5432,
-      username: '<username>',
-      password: '<password>',
-      database: '<database>',
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Row, Analytics],
-      synchronize: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
   ],
   controllers: [AppController],
