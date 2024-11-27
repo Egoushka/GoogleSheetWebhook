cc11743825fa5be14b5dc593699bd29fcc8b6e9c
Egoushka <egorgrabovskij@gmail.com>
2024-11-27 19:30:38 +0200

    fix

--- src/repositories/analytics.repository.ts
+++ src/repositories/analytics.repository.ts
@@ -1,4 +1,3 @@
-// analytics.repository.ts
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
--- src/repositories/row.repository.ts
+++ src/repositories/row.repository.ts
@@ -1,6 +1,8 @@
 import { DataSource, Repository } from 'typeorm';
 import { Row } from '../models/row.model';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class RowRepository extends Repository<Row> {
   constructor(private dataSource: DataSource) {
     super(Row, dataSource.createEntityManager());
